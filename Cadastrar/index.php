568734a668ea506b2fadf68fd1b820d7831f18e5
Adriano Martiliano <adrianomartiliano@gmail.com>
2021-10-08 13:15:56 -0300

    Criação dos arquivos

---
+++ Cadastrar/index.php
@@ -0,0 +1,13 @@
+<<html>
+    <head> 
+        <meta charset="utf-8">
+        <title>Cadastro de Cliente</title>
+    </head>
+    <body>
+        
+        <?php
+
+        ?>
+
+    </body>
+</html>
